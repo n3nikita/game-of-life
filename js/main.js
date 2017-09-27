e09eb06de747612a31d95b2f08af7f97c37c6243
Nikita <nbandaruk@gmail.com>
2017-09-27 08:18:37 +0300

    Update. Now it works with steps.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var matrix = [],
-    height = 90,
-    width = 120;
+    makeActive = [],
+    height = 50,
+    width = 50;
 
 
 //creating table
 $("#btnCreate").click(function () {
-    mainArray = [];
 
     for (var i = 0; i < height; i++){
         matrix[i] = [];
@@ -16,6 +16,7 @@ $("#btnCreate").click(function () {
         }
     }
 
+    $(".table-game").css("width", width*10).css("height", height*10);
     $("#btnStart").prop('disabled', false);
 });
 
@@ -26,7 +27,84 @@ $(".table-game").on("click", "td", function () {
 
 //TODO: write some code here
 $("#btnStart").click(function () {
-    // if (matrix[1][1].hasClass("active")) {
-    //     alert("IT WORKS!");
-    // }
+    checkCell();
 });
+
+function checkCell(){
+    makeActive=[];
+    for(var i = 1; i < width-1; i++){
+        for(var j = 1; j < height-1; j++){
+
+            checkNeighborhood(i,j);
+        }
+    }
+
+    for(var k = 0; i < makeActive.length; k++){
+        changeCell(makeActive[k]);
+    }
+}
+
+//check alive
+function alive(cell){
+    return cell.hasClass("active");
+}
+
+//change
+function changeCell(cell){
+    cell.toggleClass("active");
+}
+
+function checkNeighborhood(i,j){
+    var counter = 0;
+    var cell = matrix[i][j];
+
+    //left
+    if(alive(matrix[i][j-1])) {
+        counter++;
+    }
+
+    //right
+    if(alive(matrix[i][j+1])) {
+        counter++;
+    }
+
+    //down
+    if(alive(matrix[i-1][j])) {
+        counter++;
+    }
+
+    //up
+    if(alive(matrix[i+1][j])) {
+        counter++;
+    }
+
+    //down_left
+    if(alive(matrix[i+1][j-1])) {
+        counter++;
+    }
+
+    //down_right
+    if(alive(matrix[i+1][j+1])) {
+        counter++;
+    }
+
+    //up_left
+    if(alive(matrix[i-1][j-1])) {
+        counter++;
+    }
+
+    //up_right
+    if(alive(matrix[i-1][j+1])) {
+        counter++;
+    }
+
+
+
+    if (counter == 3 && !alive(cell)){
+        makeActive.push(cell);
+    }
+
+    if ((counter < 2 || counter > 3) && alive(cell)){
+        makeActive.push(cell);
+    }
+}
